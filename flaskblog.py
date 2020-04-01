67d9ef521a6ede07de797cd9b8a2ab543161aa10
Nicolas <nikkolasnv1997@gmail.com>
2020-04-01 19:13:00 -0300

    adding layouts

---
+++ flaskblog.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template as render
+
+app = Flask(__name__)
+
+post = [
+    {
+        'author': 'Nicolas',
+        'content': 'Hello'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render('home.html', posts=post)
+
+@app.route('/about')
+def about():
+    return render('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
