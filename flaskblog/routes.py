61a88dc013a7371d0a6882971bf62eda0be5a80b
Nicolas <nikkolasnv1997@gmail.com>
2020-04-02 23:06:49 -0300

    profile picture

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,7 @@
 
+import secrets
+import os
+from PIL import Image
 from flask import url_for, flash, redirect, request
 from flask import render_template as render
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -6,13 +9,7 @@ from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
-
-post = [
-    {
-        'author': 'Nicolas',
-        'content': 'Hello'
-    }
-]
+post={}
 
 @app.route('/')
 @app.route('/home')
@@ -57,11 +54,27 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, fext = os.path.splitext(form_picture.filename)
+    picture_name = random_hex + fext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_name)
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_name
+
+
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
